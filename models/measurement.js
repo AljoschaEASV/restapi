42a3f7c2ad91ebb4acaf8ab920223d022b02f74f
Shadeschou <capper2650@hotmail.com>
2021-03-23 12:34:44 +0100

    Updating models and routes

--- models/measurement.js
+++ models/measurement.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose')
 
 const measurementSchema = new mongoose.Schema( {
-    measureId: {
-        type: String,
-        required: true
-    },
-    airHumidity: {
-        type: Number,
-        required: true
-    },
-    temperature: {
-        type: String
-    },
-    date_time: {
-        type: Date,
-        required: true
-    },
-    Identify_measureID: {
-        type: Boolean
-    }
+        plantId:{
+            type: String,
+            required: true
+        },
+        measureId: {
+            type: String,
+            required: true
+        },
+        airHumidity: {
+            type: Number,
+            required: true
+        },
+        temperature: {
+            type: String
+        },
+        date_time: {
+            type: Date,
+            required: true
+        },
+        Identify_measureID: {
+            type: Boolean
+        }
 })
 
 
